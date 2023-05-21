e14e75791a1c0f4f2515b6e38dfb64cc446cb271
SoraTenshi <dream@neoncity.dev>
2023-05-21 22:28:03 +0200

    Fix the build

--- build.zig
+++ build.zig
@@ -1,28 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    const win = b.dependency("zigwin32", .{});
-    const lib = b.addStaticLibrary(.{
-        .name = "zig-bait",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
+    _ = b.addModule("zig-bait", .{
+        .source_file = .{ .path = "bait.zig" },
     });
-    lib.addModule("zigwin32", win.module("zigwin32"));
-    lib.addLibraryPath("src/fn_ptr");
-
-    b.installArtifact(lib);
-
-    const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    const run_main_tests = b.addRunArtifact(main_tests);
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&run_main_tests.step);
 }
