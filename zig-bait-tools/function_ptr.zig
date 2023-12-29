51806247c0261563268d529e8c84c3e1cdeab68c
SoraTenshi <dream@neoncity.dev>
2023-12-29 02:54:51 +0100

    Update

--- zig-bait-tools/function_ptr.zig
+++ zig-bait-tools/function_ptr.zig
@@ -9,7 +9,13 @@ pub fn RestoreFunctionType(comptime option_type: type) type {
 }
 
 pub inline fn checkIsFnPtr(fun: anytype) void {
-    if (!std.meta.trait.isConstPtr(@TypeOf(fun)) and @typeInfo(@typeInfo(@TypeOf(fun)).Pointer.child) != .Fn) {
-        @compileError("Expected fun to be a pointer to a function, got: " ++ @typeName(@TypeOf(fun)));
+    switch (@typeInfo(@TypeOf(fun))) {
+        .Pointer => |ptr| {
+            switch (@typeInfo(ptr.child)) {
+                .Fn => {},
+                else => @compileError("Expected fun to be a pointer to a function, got: " ++ @typeName(@TypeOf(fun))),
+            }
+        },
+        else => @compileError("Expected fun to be a pointer to a function, got: " ++ @typeName(@TypeOf(fun))),
     }
 }
