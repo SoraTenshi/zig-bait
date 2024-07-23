384145396a58f9a2b5eb7acc8d40142289548c58
SoraTenshi <dream@neoncity.dev>
2024-07-23 23:52:56 +0200

    refactor: eliminate all uses of usingnamespace

--- zig-bait-tools/zig-bait-tools.zig
+++ zig-bait-tools/zig-bait-tools.zig
@@ -1,11 +1,25 @@
 /// Vtable utilities
-pub usingnamespace @import("vtable.zig");
+const vtable = @import("vtable.zig");
+pub const Vtable = vtable.Vtable;
+pub const AbstractClass = vtable.AbstractClass;
+pub const addressToVtable = vtable.addressToVtable;
 
 /// Protection utilities
-pub usingnamespace @import("protect.zig");
+const protect = @import("protect.zig");
+pub const Address = protect.Address;
+pub const Flags = protect.Flags;
+pub const getFlags = protect.getFlags;
+pub const setNewProtect = protect.setNewProtect;
 
 /// Function utilities
-pub usingnamespace @import("function_ptr.zig");
+const fn_ptr = @import("function_ptr.zig");
+pub const HookFunctionType = fn_ptr.HookFunctionType;
+pub const RestoreFunctionType = fn_ptr.RestoreFunctionType;
+pub const checkIsFnPtr = fn_ptr.checkIsFnPtr;
 
 /// Opcodes
-pub usingnamespace @import("assembler.zig");
+const assembler = @import("assembler.zig");
+pub const Opcodes = assembler.Opcodes;
+pub const Register = assembler.Register;
+pub const ptrSize = assembler.ptrSize;
+pub const addressToBytes = assembler.addressToBytes;
